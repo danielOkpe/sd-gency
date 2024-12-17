82cba38eb43aac3b2686c797a2408d27e3b8714c
danielOkpe <145658821+danielOkpe@users.noreply.github.com>
2024-12-17 07:22:41 +0100

    home header

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,13 @@
             <!-- Zone avec carré rouge au centre -->
             <div class="center"></div>
 
-            <!-- Zone pour les carrés bleus -->
-            <div class="rand" style="top: 0%; left: 0;"></div>
-            <div class="rand" style="top: 32%; left: 15%;"></div>
-            <div class="rand" style="bottom: 0%; left: 5%;"></div>
-
-            <div class="rand" style="top: 2%; right: 35%;"></div>
-            <div class="rand" style="bottom: 30%; right: 17%;"></div>
-            <div class="rand" style="bottom: 0%; right: 40%;"></div>
+            <!-- Carrés bleus espacés autour du carré rouge -->
+            <div class="rand" style="top: 0%; left: 50%; transform: translateX(-50%);"></div> <!-- Haut -->
+            <div class="rand" style="top: 10%; left: 10%;"></div> <!-- Haut-gauche -->
+            <div class="rand" style="bottom: 10%; left: 10%; transform: translateY(-50%);"></div> <!-- Milieu-gauche -->
+            <div class="rand" style="top: 10%; right: 10%;"></div> <!-- Bas-gauche -->
+            <div class="rand" style="bottom: 0%; left: 50%; transform: translateX(-50%);"></div> <!-- Bas -->
+            <div class="rand" style="bottom: 10%; right: 10%; transform: translateY(-50%);"></div> <!-- Milieu-droit -->
         </section>
     </header>
 </template>
@@ -43,18 +42,15 @@ header {
 
 .rand {
     position: absolute;
-    width: 370px;
-    height: 270px;
+    width: 350px;
+    height: 250px;
     background-color: blue;
     border-radius: 30px;
-}
-
-div{
     transition: all 0.3s ease-in-out;
     cursor: pointer;
 }
 
-div:hover {
+.rand:hover {
     scale: 1.1;
 }
 </style>
